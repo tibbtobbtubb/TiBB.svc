c3ce237cd8e78eebf0d0dfab47e7d7d3e65d4212
Kevin Purnama <tibbtobbtubb@gmail.com>
2015-06-11 15:13:24 +1000

    Test upload
    I’m alive text

---
+++ public_html/nodejs/main.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+console.log("I'm alive")
+
+// var clc = require("cli-color")
+// console.log(clc.green("I'm green"))
+
+
